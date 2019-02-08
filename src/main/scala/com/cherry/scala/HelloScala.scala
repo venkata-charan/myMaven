b267a71b6d0715e3c68017d3f1a8a9f67bb6bc65
venkata-charan <charanraj.lv@gmail.com>
2019-02-08 06:37:33 +0530

    My first change

--- src/main/scala/com/cherry/scala/HelloScala.scala
+++ src/main/scala/com/cherry/scala/HelloScala.scala
@@ -5,6 +5,7 @@ object HelloScala {
   def main(ary:Array[String]):Unit = {
 
     println("Hi scala, how are you doing with maven")
+    println("Abbo, now you pull and push of git , now lets rock it")
 
   }
 
